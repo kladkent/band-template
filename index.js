33a87fd62f51582b5f20bd11898dc712aca606b1
kladkent <41990273+kladkent@users.noreply.github.com>
2020-12-18 12:08:06 +0800

    Add files via upload
    1. Add SVG icons for bar menu, arrow & search
    2. Update JavaScript code
    3. Update HTML code
    4. Update CSS code

--- index.js
+++ index.js
@@ -18,6 +18,23 @@ function showSlides() {
 	}    
 			  
 	slides[slideIndex-1].style.display = "block";  
-	setTimeout(showSlides, 10000);
+	setTimeout(showSlides, 10000); // Change image every 2 seconds
 			
+}
+
+
+function arrowDarkIcon(){
+	document.getElementsByClassName("arrow-icon")[0].src="icons/arrow-down-dark.svg";
+}
+
+function arrowWhiteIcon(){
+	document.getElementsByClassName("arrow-icon")[0].src="icons/arrow-down-white.svg";
+}
+
+function barDarkIcon(){
+	document.getElementsByClassName("bar-icon")[0].src="icons/menu-bar-dark.svg";
+}
+
+function barWhiteIcon(){
+	document.getElementsByClassName("bar-icon")[0].src="icons/menu-bar-white.svg";
 }
